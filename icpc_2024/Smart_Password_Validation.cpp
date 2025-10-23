ec50ee314dd4497b7e057d83759fa62b2749cb9d
Andrew Wang <someone.zip@gmail.com>
2025-10-22 20:58:31 -0400

    sad

--- icpc_2024/Smart_Password_Validation.cpp
+++ icpc_2024/Smart_Password_Validation.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+map<char, char> leftShift;
+map<char, char> rightShift;
 
 void solve() {
     int r, c;
@@ -13,6 +15,40 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
+
+    leftShift['`'] = '1';
+    leftShift['1'] = '2';
+    leftShift['2'] = '3';
+    leftShift['3'] = '4';
+    leftShift['4'] = '5';
+    leftShift['5'] = '6';
+    leftShift['6'] = '7';
+    leftShift['~'] = '!';
+    leftShift['!'] = '@';
+    leftShift['@'] = '#';
+    leftShift['#'] = '$';
+    leftShift['$'] = '%';
+    leftShift['%'] = '^';
+    leftShift['^'] = '&';
+    leftShift['q'] = 'w';
+    leftShift['a'] = 's';
+    leftShift['z'] = 'x';
+    leftShift['w'] = 'e';
+    leftShift['s'] = 'd';
+    leftShift['x'] = 'c';
+    leftShift['e'] = 'r';
+    leftShift['d'] = 'f';
+    leftShift['c'] = 'v';
+    leftShift['r'] = 't';
+    leftShift['f'] = 'g';
+    leftShift['v'] = 'b';
+    leftShift['t'] = 'y';
+    leftShift['g'] = 'h';
+    leftShift['b'] = 'n';
+    // remember to_lower and to_upper since we are only mapping lowercase
+    
+
+
 
     solve();
 }
